1c73aa80643fd41cb109c41fab167d37275f9d80
ritsuke <ritsuke@gmail.com>
2022-08-22 17:27:00 -0500

    Add lang attribute

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 const Document = () => {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
         <link rel="shortcut icon" href="/favicon.ico" />
 
