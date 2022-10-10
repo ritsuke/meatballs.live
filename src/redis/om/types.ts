8774b18cf47c13007e2598201ccfa5aaf348e448
fred <ritsuke@gmail.com>
2022-10-10 08:08:16 -0500

    Redirect site to main (disabled state)

--- src/redis/om/types.ts
+++
@@ -1,8 +0,0 @@
-import { Entity, Repository } from 'redis-om'
-
-export type MeatballObjectType = 'Story' | 'User' | 'Comment'
-
-export type OmObjectReturnType<T extends Entity> = {
-  repository?: Repository<T>
-  closeRepository: () => Promise<void>
-}
--- src/utils/index.ts
+++
@@ -1,2 +0,0 @@
-export const removeSpecialCharacters = (value: string) =>
-  value.replace(/[^a-zA-Z0-9 ]/g, '')
